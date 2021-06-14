ed38a597675fd8e6dfa1dc1fe9dd503da1acf43f
yagnic <30473493+yagnic@users.noreply.github.com>
2021-06-14 13:01:48 +0530

    added apiviews

---
+++ quiz/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import Quiz, QuizQuestion, RandomQuestion
+
+app_name = 'quiz'
+
+urlpatterns = [
+    path('', Quiz.as_view(), name='quiz'),
+    path('r/<str:topic>', RandomQuestion.as_view(), name='randomquestion'),
+    path('q/<str:topic>', QuizQuestion.as_view(), name='questions')
+
+
+
+]
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from rest_framework import generics
+from rest_framework.serializers import Serializer
+from .models import Quizzes, Question
+from .serializers import QuizSerializer, RandomQuestionSerializer, QuestionSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class Quiz(generics.ListAPIView):
+
+    serializer_class = QuizSerializer
+    queryset = Quizzes.objects.all()
+
+
+class RandomQuestion(APIView):
+
+    def get(self, request, format=None, **kwargs):
+        question = Question.objects.filter(
+            quiz__title=kwargs['topic']).order_by('?')[:1]
+        serializer = RandomQuestionSerializer(question, many=True)
+
+        return Response(serializer.data)
+
+
+class QuizQuestion(APIView):
+
+    def get(self, request, format=None, **kwargs):
+        question = Question.objects.filter(quiz__title=kwargs['topic'])
+        serializer = QuestionSerializer(question, many=True)
+        return Response(serializer.data)
